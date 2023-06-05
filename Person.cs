35d765d5e825c4af95981e4c86229d8e69950b92
YaroslavLevchenko <Yarik.levchenko.net@gmail.com>
2023-06-05 17:55:22 +0300

    load

---
+++ Person.cs
@@ -0,0 +1,74 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    public class Person
+    {
+        string name;
+
+        DateTime birthYear;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public DateTime BirthYear
+        {
+            get => birthYear;
+            set => birthYear = value;
+        }
+        public Person() { }
+
+        public Person(string name, DateTime birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        public int Age()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - BirthYear.Year;
+            return age;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Please, type your name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Please, type your date of birth: ");
+            BirthYear = DateTime.Parse(Console.ReadLine());
+        }
+
+        public void ChangeName()
+        {
+
+            DateTime today = DateTime.Today;
+            const string changedName = "Very young";
+            if (today.Year - birthYear.Year < 16)
+            {
+                Name = changedName;
+                Console.WriteLine("New name is: " + $"{Name}"); 
+            }
+            else Console.WriteLine("Name is : " + $"{Name}");
+        }
+
+        public override string ToString()
+        {
+            return $"name: {name}, birth year: {birthYear.ToShortDateString()}, age: {Age()}";
+        }
+        public void Output()
+        {
+            Console.WriteLine("Info about the person: " + $"{ToString()}") ;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person[] people = new Person[6]; 
+
+            for(int i = 0; i<people.Length; i++)
+            {
+                people[i] = new Person();
+                people[i].Input();
+            }
+            foreach (var person in people)
+            {
+                person.Output();
+            }
+
+            foreach(var person in people)
+            {
+                person.ChangeName();
+            }
+            //All new info with the changed name
+            foreach (var person in people)
+            {
+                person.Output();
+            }
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                for (int j = i + 1; j < people.Length; j++)
+                {
+                    if (people[i].Name == people[j].Name)
+                    {
+                        Console.WriteLine(people[i].ToString());
+                        Console.WriteLine(people[j].ToString());
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
